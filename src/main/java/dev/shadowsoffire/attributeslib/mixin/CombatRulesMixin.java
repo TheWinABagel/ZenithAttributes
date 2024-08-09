3d6e10e6462ef441a7d87c87ee7fead98a6914f3
Ash (Bagel) <ash@rjohnston.net>
2024-08-08 19:26:56 -0500

    remove log spam

--- src/main/java/dev/shadowsoffire/attributeslib/mixin/CombatRulesMixin.java
+++ src/main/java/dev/shadowsoffire/attributeslib/mixin/CombatRulesMixin.java
@@ -18,7 +18,6 @@ public class CombatRulesMixin {
      */
     @Inject(method ="getDamageAfterMagicAbsorb", at = @At("HEAD"), cancellable = true)
     private static void zenith_attributes$getDamageAfterMagicAbsorb(float damage, float protPoints, CallbackInfoReturnable<Float> cir) {
-        AttributesLib.LOGGER.warn("Overriding getDamageAfterMagicAbsorb!");
         cir.setReturnValue(damage * ALCombatRules.getProtDamageReduction(protPoints));
     }
 
@@ -28,7 +27,6 @@ public class CombatRulesMixin {
     @Inject(method ="getDamageAfterAbsorb", at = @At("HEAD"), cancellable = true)
     private static void zenith_attributes$getDamageAfterAbsorb(float damage, float totalArmor, float toughnessAttribute, CallbackInfoReturnable<Float> cir) {
         AttributesLib.LOGGER.trace("Invocation of CombatRules#getDamageAfterAbsorb is bypassing armor pen.");
-        AttributesLib.LOGGER.warn("Overriding getDamageAfterAbsorb!");
         cir.setReturnValue(damage * ALCombatRules.getArmorDamageReduction(damage, totalArmor));
     }
 }
